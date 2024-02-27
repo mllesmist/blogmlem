b6004951647f5a8faaf91762f858fe5d392fc4d8
haidang1201 <haidang1201hd@gmail.com>
2024-02-27 23:25:35 +0100

    fix user register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['Email'], message: 'There is already an account with this Email')]
+// #[UniqueEntity(fields: ['Email'], message: 'There is already an account with this Email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
