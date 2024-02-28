9b41bedafb420e923d1072def1c6b35c5a251da7
Nguyen Lan Anh <uyencucucu@LE-Phuong-Uyen.local>
2024-02-28 01:31:25 +0100

    projet symfony stage

--- migrations/Version20240224004313.php
+++ migrations/Version20240224004313.php
@@ -22,7 +22,7 @@ final class Version20240224004313 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
+       // $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
@@ -30,6 +30,6 @@ final class Version20240224004313 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
         $this->addSql('DROP TABLE reset_password_request');
-        $this->addSql('ALTER TABLE user DROP is_verified');
+       // $this->addSql('ALTER TABLE user DROP is_verified');
     }
 }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -42,16 +42,10 @@ class RegistrationController extends AbstractController
             );
 
             $entityManager->persist($user);
-            $entityManager->flush();
+           // $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('no-reply@blogmlem.com', 'No Reply'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+           
             // do anything else you need here, like send an email
 
             return $this->redirectToRoute('app_home');
